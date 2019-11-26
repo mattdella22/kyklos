36ee3214f92ca3ba985379abb5128982f56036a9
mattdella <37800031+mattdella22@users.noreply.github.com>
2019-11-26 16:11:16 +0100

    aggiunto sensori

--- js/main.js
+++ js/main.js
@@ -1,42 +1,77 @@
-//mattia
 let rings = [];
-
+var alpha = 0; // Orientation around Z axis
+var beta = 0; // Orientation around X axis
+var gamma = 0; // Orientation around Y axis
+let start = false;
+let factor = 1.2
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    for (let i = 0; i < 25; i++) {
-        rings[i] = new Ring(15 + i*4, 0)
+
+    for (let i = 0; i < 13; i++) {
+        rings[i] = new Ring(11 + i * 18, i * 0.1414)
     }
 
     background(0);
 }
 
 function draw() {
-    background(0,0,0,40);
-    //DEBUG
-    /*
-    let txt = "ring.current =" + ring.current;
-    txt += "<br> ring.angle =" + ring.angle;
-    txt += "<br>ring.pathHistory.length =" + ring.pathHistory.length + "\n";
-    txt += "<br>ring.pathHistory[0] =" + ring.pathHistory[0] + "\n";
-    */
+    if (touches.length>0) {
+        background(0, 0, 0, 15 / factor)
+        factor = factor * 2.4;
+    } else {
+        background(0, 0, 0, 40);
+    }
 
     translate(width / 2, height / 2);
-    scale(2);
+
+    scale(1);
+
+    /*
+    alpha = alpha-45;//tolgo 45 gradi ad alpha, per fare cominciare lo 0 prima...
+    let constrainAlpha = constrain(alpha, 0, 90);
+    let newAlpha = map(constrainAlpha, 0, 90, 0, 50);
+    */
+
+
+    //let newGamma = map(gamma, -90, 90, -20, 52);
+
 
     for (let i = 0; i < rings.length; i++) {
         rings[i].rotate();
-        rings[i].update();
+        rings[i].update(-i * 0.1414);
         rings[i].erase();
         rings[i].show();
 
     }
 
+    //DEBUG
+    let txt = "rings[0].current =" + rings[0].current;
+    txt += "<br> acceleration z =" ;
 
-    //debug(txt);
+    debug(txt);
 }
 
 function debug(val) {
     document.querySelector("#debug").innerHTML = "Debug : <br>" + val;
+}
+
+window.addEventListener("deviceorientation", function(event) {
+    alpha = event.alpha;
+    beta = event.beta - 45;
+    gamma = event.gamma;
+});
+
+
+function touchStarted(event) {
+    console.log(event);
+
+}
+
+document.ontouchmove = function(e) {
+    e.preventDefault();
+}
+document.ontouchmove = function(e) {
+    e.preventDefault();
 }
--- js/ring.js
+++ js/ring.js
@@ -7,30 +7,27 @@ class Ring {
     }
 
     update() {
-    	//prima di aggiornare inserisco nella storia, il punto
-    	//console.log(this.pathHistory[0]);
-
-        this.x = this.r * cos(this.angle);
+        this.x = this.r  * cos(this.angle);
         this.y = this.r * sin(this.angle);
 
         this.current.x = this.x;
         this.current.y = this.y;
 
-    	this.pathHistory.push(this.current);
+        this.pathHistory.push(this.current);
     }
 
     show() {
         //strokeWeight(5);
-        stroke(255,255,255);
-        strokeWeight(2)
+        stroke(255, 255, 255);
+        strokeWeight(6)
         //fill(255,255,255,180);
         noFill();
         beginShape();
-        for(let v of this.pathHistory){
+        for (let v of this.pathHistory) {
             vertex(v.x, v.y)
         }
         endShape();
-        this.current = createVector();
+        this.current = createVector(); //perchè non funziona, senza questa linea?
     }
 
 
@@ -38,10 +35,8 @@ class Ring {
         this.angle += 0.05;
     }
 
-    erase(){
-    	//Cancello dall'history i punti partendo dal fondo (pathHistory[pathHistory.lenght])
-    	//"rompo"la forma all'ultimo punto, così che avrò sempre una linea aperta...
-        if(this.angle > radians(270)){
+    erase() {
+        if (this.angle > radians(270)) {
             this.pathHistory.shift();
         }
     }
